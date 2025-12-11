5bae7eb02a88b8339c398a761da0919d267eec3c
pandusudo <pandusudo@gmail.com>
2025-12-11 14:41:18 +0700

    fix: adjust email auth secure

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -1,7 +1,15 @@
 import nodemailer from "nodemailer";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: false,
+  secure: isProduction,
+  auth: isProduction
+    ? {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      }
+    : undefined,
 });
